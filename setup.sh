d63c8237f225d4a99e1f9b4a3219905ef79bdf02
Thomas Newman <tnewman@noreply.github.com>
2018-08-25 11:47:44 -0400

    Updated Android Studio Version

--- setup.sh
+++ setup.sh
@@ -28,7 +28,7 @@ tarballs=(
 )
 
 zips=(
-    https://dl.google.com/dl/android/studio/ide-zips/3.1.2.0/android-studio-ide-173.4720617-linux.zip
+    https://dl.google.com/dl/android/studio/ide-zips/3.1.4.0/android-studio-ide-173.4907809-linux.zip
 )
 
 bins=(
