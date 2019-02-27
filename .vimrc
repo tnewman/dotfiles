4c2b3f38cdd19d6f96f6b32afc89f37bfa6f64b2
Thomas Newman <tnewman@users.noreply.github.com>
2019-02-26 23:30:30 -0500

    Updated with new configuration

--- .vimrc
+++ .vimrc
@@ -1,10 +1,4 @@
-execute pathogen#infect()
-
 syntax enable
-
-set background=dark
-colorscheme solarized
-hi Normal guibg=NONE ctermbg=NONE
 
 filetype plugin indent on
 set tabstop=4
--- setup.sh
+++ setup.sh
@@ -1,101 +1,91 @@
 #!/bin/bash
 set -e
 
-ppas=(
-    ppa:cwchien/gradle
+app_directory=~/.dotfiles
+bin_directory=$app_directory/bin
+distro=$(lsb_release -s -c)
+node_version=10
+script_directory=$(pwd)
+
+apt_keys=(
+    "https://deb.nodesource.com/gpgkey/nodesource.gpg.key"
+    "https://download.docker.com/linux/ubuntu/gpg"
+    "https://packages.cloud.google.com/apt/doc/apt-key.gpg"
+)
+
+repos=(
+    "deb https://apt.kubernetes.io/ kubernetes-xenial main"
+    "deb https://deb.nodesource.com/node_${node_version}.x $distro main"
+    "deb-src https://deb.nodesource.com/node_${node_version}.x $distro main"
+    "deb https://download.docker.com/linux/ubuntu $distro stable"
 )
 
 packages=(
+    bridge-utils
     build-essential
-    dconf-cli
+    containerd.io
     curl
+    default-jdk
     docker-ce
+    docker-ce-cli
+    gconf2
     git
-    gradle
+    jq
+    kubectl
+    libvirt-clients
+    libvirt-daemon-system
     nodejs
-    openjdk-11-jdk
     python3-pip
+    python3-venv
+    qemu-kvm
     ubuntu-restricted-extras
     vim
-    virtualbox
-    wget
-)
-   
-
-tarballs=(
-    https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.11.4231.tar.gz
-)
-
-zips=(
-    https://dl.google.com/dl/android/studio/ide-zips/3.1.4.0/android-studio-ide-173.4907809-linux.zip
+    virt-manager
 )
 
 bins=(
-    https://github.com/kubernetes/minikube/releases/download/v0.28.2/minikube-linux-amd64
-    https://storage.googleapis.com/kubernetes-release/release/v1.11.2/bin/linux/amd64/kubectl
-)
+    https://storage.googleapis.com/minikube/releases/latest/docker-machine-driver-kvm2
+    https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64)
 
-git_repos=(
-    https://github.com/Anthony25/gnome-terminal-colors-solarized.git
-    https://github.com/altercation/vim-colors-solarized.git
-)
+function install_apt_keys() {
+    echo "Installing APT Keys"
 
-python3_packages=(
-    virtualenv
-)
-
-app_directory=~/.dotfiles
-bin_directory=$app_directory/bin
-script_directory=$(pwd)
-
-function install_ppas() {
-    echo "Installing PPAs"
-    
-    for ppa in ${ppas[@]}
+    for apt_key in ${apt_keys[@]}
     do
-        echo "====> Installing $ppa"
-        sudo add-apt-repository -y $ppa
+        echo "====> Installing $apt_key"
+        wget -qO- $apt_key | sudo apt-key add -
     done
 }
 
-function install_docker_repo() {
-    echo "Installing Docker Repository"
+function install_repos() {
+    echo "Installing APT Repos"
 
-    echo "====> Installing Prerequisites"
-    sudo apt-get install -qq -y apt-transport-https ca-certificates curl \
-        software-properties-common
+    sudo rm -f /etc/apt/sources.list.d/dotfiles.list
 
-    echo "====> Adding Docker GPG Key"
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-    echo "====> Adding Docker Repository"
-    sudo add-apt-repository \
-           "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-           $(lsb_release -cs) \
-           edge"
+    for repo in "${repos[@]}"
+    do
+        echo "====> Installing $repo"
+        echo "$repo" | sudo tee -a /etc/apt/sources.list.d/dotfiles.list > /dev/null
+    done
 }
 
 function update() {
     echo "Updating Ubuntu"
-    
+
     echo "====> Updating Package Cache"
     sudo apt-get update
-    
+
     echo "====> Updating Packages"
     sudo apt-get dist-upgrade -y
-    
+
     echo "====> Removing Obsolete Packages"
     sudo apt-get autoremove -y
 }
 
 function install_packages() {
     echo "Installing Packages"
-    
-    for package in ${packages[@]}
-    do
-        echo "====> Installing $package"
-        sudo apt-get install -qq -y $package
-    done
+    sudo apt-get install -qq -y ${packages[@]}
+
 }
 
 function initialize_app_directory() {
@@ -109,46 +99,6 @@ function initialize_app_directory() {
 
     echo "====> Creating $bin_directory"
     mkdir -p $bin_directory
-}
-
-function install_tarballs() {
-    echo "Installing Tarballs"
-
-    echo "====> Moving to $app_directory"
-    cd $app_directory
-
-    for tarball_url in ${tarballs[@]}
-    do
-        tarball=${tarball_url##*/}
-        echo "====> Downloading $tarball_url"
-        wget --quiet $tarball_url
-        
-        echo "====> Extracting $tarball"
-        tar -xf $tarball
-        
-        echo "====> Removing $tarball"
-        rm $tarball
-    done
-}
-
-function install_zips() {
-    echo "Installing Zips"
-    
-    echo "====> Moving to $app_directory"
-    cd $app_directory
-
-    for zip_url in ${zips[@]}
-    do
-        zip=${zip_url##*/}
-	    echo "====> Downloading $zip_url"
-	    wget --quiet $zip_url
-
-	    echo "====> Extracting $zip"
-	    unzip -qq $zip
-
-	    echo "====> Removing $zip"
-	    rm $zip
-    done
 }
 
 function install_bins() {
@@ -170,96 +120,51 @@ function install_bins() {
     mv $bin_directory/minikube-linux-amd64 minikube
 }
 
-function install_git_repos() {
-    echo "Installing Git Repos"
-    
-    echo "====> Moving to $app_directory"
-    cd $app_directory
-    
-    for repo_url in ${git_repos[@]}
-    do
-        echo "====> Cloning $repo_url"
-	git clone -q $repo_url
-    done
-}
-
-function install_commands() {
-    echo "Installing Commands"
-
-    for cmd in ${commands[@]}
-    do
-        echo "====> Installing $cmd"
-    done
-}
-
-function install_python3_packages() {
-    echo "Installing Python 3 Packages"
-    
-    for package in ${python3_packages[@]}
-    do
-        echo "====> Installing $package"
-        pip3 install -q --user $package
-    done
-}
-
-function install_terminal_profile() {
-    echo "====> Create SolarizedDark Terminal Profile"
-    dconf write /org/gnome/terminal/legacy/profiles:/:be8a3d5c-f849-4996-817d-bb694f75fca5/visible-name "'Solarized Dark'"
-    dconf write /org/gnome/terminal/legacy/profiles:/list "['be8a3d5c-f849-4996-817d-bb694f75fca5']"
-
-    echo "====> Set Default Terminal Profile to Solarized Dark"
-    dconf write /org/gnome/terminal/legacy/profiles:/default "'be8a3d5c-f849-4996-817d-bb694f75fca5'"
-    
-    echo "====> Installing Solarized Terminal Colors"
-    $app_directory/gnome-terminal-colors-solarized/install.sh \
-        --profile "Solarized Dark" --scheme dark \
-        --skip-dircolors
-
-    echo "====> Setting Terminal Transparency"
-    dconf write /org/gnome/terminal/legacy/profiles:/:be8a3d5c-f849-4996-817d-bb694f75fca5/use-theme-transparency false
-    dconf write /org/gnome/terminal/legacy/profiles:/:be8a3d5c-f849-4996-817d-bb694f75fca5/use-transparent-background true
-    dconf write /org/gnome/terminal/legacy/profiles:/:be8a3d5c-f849-4996-817d-bb694f75fca5/background-transparency-percent 10
-}
-
-function install_vim_profile() {
-    echo "====> Installing Pathogen for Vim"
-    mkdir -p ~/.vim/autoload
-    wget --quiet -O $app_directory/pathogen.vim https://tpo.pe/pathogen.vim
-    ln -f -s $app_directory/pathogen.vim ~/.vim/autoload/pathogen.vim
-    
-    echo "====> Installing .vimrc"
-    cp $script_directory/.vimrc $app_directory
-    ln -f -s $app_directory/.vimrc ~/.vimrc
-    
-    echo "====> Installing Vim Solarized Colors"
-    mkdir -p ~/.vim/bundle
-    ln -f -s $app_directory/vim-colors-solarized ~/.vim/bundle/vim-colors-solarized
+function install_jetbrains_toolbox() {
+    echo "Installing Jetbrains Toolbox"
+    if [ -d ~/.local/share/JetBrains/Toolbox ]; then
+        echo "====> Jetbrains Toolbox Already Exists (Skipping Installation)"
+    else
+        echo "====> Installing Jetbrains Toolbox"
+        toolbox_url=$(wget -qO- "https://data.services.jetbrains.com//products/releases?code=TBA&latest=true&type=release" | jq -r '.TBA[0].downloads.linux.link')
+        wget -qO- $toolbox_url | tar -zxO --wildcards --no-anchored 'jetbrains-toolbox' > /tmp/jetbrains-toolbox
+        chmod +x /tmp/jetbrains-toolbox
+        /tmp/jetbrains-toolbox &
+    fi
 }
 
 function install_configuration() {
     echo "Installing Configuration"
-    install_terminal_profile
-    install_vim_profile
     
+    echo "====> Installing .vimrc"
+    cp $script_directory/.vimrc $app_directory
+    ln -f -s $app_directory/.vimrc ~/.vimrc
+
     echo "====> Installing .bashrc"
     cp $script_directory/.bashrc $app_directory
     rm ~/.bashrc
     ln -f -s $app_directory/.bashrc ~/.bashrc
     source ~/.bashrc
 
+    echo "====> Setting Terminal Transparency"
+    gconftool-2 --set /apps/gnome-terminal/profiles/Default/background_darkness --type=float 0.50
+
     echo "====> Add User to Docker Group"
     sudo usermod -aG docker $USER
+    
+    echo "====> Add User to libvirt Group"
+    sudo usermod -aG libvirt $USER
+    
+    echo "====> Enable Minikube KVM Driver"
+    minikube config set vm-driver kvm2
 }
 
-install_ppas
-install_docker_repo
+install_apt_keys
+install_repos
 update
 install_packages
 initialize_app_directory
-install_tarballs
-install_zips
 install_bins
-install_git_repos
-install_python3_packages
+install_jetbrains_toolbox
 install_configuration
 
