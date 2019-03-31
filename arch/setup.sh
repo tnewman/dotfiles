0b22d42d21e64810837ed060f2e335e2294ad6ca
Thomas Newman <tnewman@users.noreply.github.com>
2019-03-30 22:28:36 -0400

    Create setup.sh for Arch install

---
+++ arch/setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+function is_mount_point() {
+    mountpoint $1 >/dev/null 2>&1
+    (( $? == 0 ))
+}
+
+if ! is_mount_point /mnt ; then
+    echo "Root partition should be mounted at /mnt"
+    exit 1
+fi
+
+if ! is_mount_point /mnt/boot ; then
+    echo "Boot partition should be mounted at /mnt/boot"
+    exit 1
+fi
+
+pacman -Sy --noconfirm reflector
+echo "Configuring Mirror List"
+reflector --country 'United States' --protocol https --age 24 --sort rate --save /etc/pacman.d/mirrorlist
+pacstrap /mnt amd-ucode base intel-ucode kate konsole firefox networkmanager plasma reflector sddm sudo
+
+genfstab -U /mnt > /mnt/etc/fstab
+
+cp chroot.sh /mnt/chroot.sh
+arch-chroot /mnt ./chroot.sh
+rm chroot.sh
