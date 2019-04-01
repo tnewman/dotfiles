533ddb82700c1732c3b100072effd03893324f7e
Thomas Newman <tnewman@users.noreply.github.com>
2019-03-31 20:02:42 -0400

    Added base-devel

--- arch/setup.sh
+++ arch/setup.sh
@@ -20,7 +20,7 @@ pacman -Sy --noconfirm reflector
 echo "Configuring Mirror List"
 reflector --country 'United States' --protocol https --age 24 --sort rate --save /etc/pacman.d/mirrorlist
 
-pacstrap /mnt amd-ucode base intel-ucode kate konsole firefox fwupd networkmanager noto-fonts \
+pacstrap /mnt amd-ucode base base-devel intel-ucode kate konsole firefox fwupd networkmanager noto-fonts \
     packagekit-qt5 plasma reflector sddm sudo ttf-dejavu ttf-croscore ttf-liberation
 
 genfstab -U /mnt > /mnt/etc/fstab
